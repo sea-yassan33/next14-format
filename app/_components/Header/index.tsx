3b95e4fe937294b4b37fc17884c0d5a9a43e4a03
sea-yassan33 <a.yasuda_tr51@me.com>
2025-01-01 07:07:33 +0900

    update250101

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,26 +1,40 @@
 import Image from "next/image";
 import Link from 'next/link';
+import { tv } from 'tailwind-variants';
 
-const twStyles = {
-  // ヘッダーのスタイル
-  headerTop:"text-gray-600 body-font bg-gray-100",
-  header01:"container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center",
-  header01Right:"flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0",
-  header01Left:"md:ml-auto flex flex-wrap items-center text-base justify-center",
-  header01titleSub:"tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
-}
 
 export default function Header(){
+  const twStayles = tv({
+    variants: {
+      style:{
+        st01:'max-w-6xl mx-auto flex justify-between items-center',
+        nav01:'hidden md:flex items-center gap-8',
+        nav02:'hover:text-muted-foreground transition-colors',
+      },
+    },
+  });
+
   return(
-    <header className={`${twStyles.headerTop}`}>
-      <div className={`${twStyles.header01}`}>
-        <div className={`${twStyles.header01Right}`}>
-          <Link href={'/'} className="text-3xl">Next.js practice</Link>
+    <header className="w-full px-6 py-4 bg-slate-300">
+      <div className={twStayles({style:'st01'})}>
+        <div className="flex flex-col">
+          <h1 className="text-xl font-light">Create Web App</h1>
+          <p className="text-sm text-muted-foreground">Sub Title</p>
         </div>
-        <div className={`${twStyles.header01Left}`}>
-          <Link href="https://zenn.dev/akfm/books/nextjs-basic-principle" className="mr-5 hover:text-gray-900" target="_blank">Next.jsについて</Link>
-          <Link href="https://www.youtube.com/watch?v=Ca1h3KUfQ5k" className="mr-5 hover:text-gray-900" target="_blank">ベストプラクティス</Link>
-        </div>
+        <nav className={twStayles({style:'nav01'})}>
+          <Link href="/" className={twStayles({style:'nav02'})}>
+            HOME
+          </Link>
+          <Link href="/works" className={twStayles({style:'nav02'})}>
+            WORKS
+          </Link>
+          <Link href="/about" className={twStayles({style:'nav02'})}>
+            ABOUT
+          </Link>
+          <Link href="/contact" className={twStayles({style:'nav02'})}>
+            CONTACT
+          </Link>
+        </nav>
       </div>
     </header>
   );
